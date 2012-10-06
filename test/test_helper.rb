db8a6bc3e2244862d958aafc76ba766616c6a09b
imoto <imoto.morefun@gmail.com>
2012-10-07 00:35:50 +0900

    add model Topic

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,13 +1,34 @@
-ENV["RAILS_ENV"] = "test"
-require File.expand_path('../../config/environment', __FILE__)
-require 'rails/test_help'
+require 'spork'
+require 'simplecov'
 
-class ActiveSupport::TestCase
-  # Setup all fixtures in test/fixtures/*.(yml|csv) for all tests in alphabetical order.
-  #
-  # Note: You'll currently still have to declare fixtures explicitly in integration tests
-  # -- they do not yet inherit this setting
-  fixtures :all
+SimpleCov.start 'rails'
 
-  # Add more helper methods to be used by all tests here...
+Spork.prefork do
+  ENV["RAILS_ENV"] = "test"
+  require File.expand_path('../../config/environment', __FILE__)
+  require 'rails/test_help'
+
+  class ActiveSupport::TestCase
+    fixtures :all
+    #def login(user_id=nil)
+    #  if user_id
+    #    @request.session[:user_id] = user_id
+    #  else
+    #    user = User.create(:twitter_id => "dummy")
+    #    @request.session[:user_id] = user.id
+    #  end
+    #end
+  end
+
+  VCR.configure do |c|
+    c.cassette_library_dir = 'test/vcr_cassettes'
+    c.allow_http_connections_when_no_cassette = true
+    c.hook_into :webmock # or :fakeweb
+  end
 end
+
+Spork.each_run do
+  load "#{Rails.root}/config/routes.rb" 
+  Dir["#{Rails.root}/app/**/*.rb"].each { |f| load f }
+end
+
