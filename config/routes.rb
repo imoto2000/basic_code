b63b7a734837ed5eec1d1d18df48a17e3c75f65c
imoto <imoto.morefun@gmail.com>
2012-10-07 00:38:50 +0900

    rm index.html

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 BasicCode::Application.routes.draw do
   get "topics/index"
 
-  get "topics/update"
+  post "topics/update"
 
   get "topics/show"
 
@@ -54,7 +54,7 @@ BasicCode::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  # root :to => 'welcome#index'
+   root :to => 'topics#index'
 
   # See how all your routes lay out with "rake routes"
 
